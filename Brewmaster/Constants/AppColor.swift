e7f90c619caa473184a53956d978307e8c2a5c95
Reza Daneshvar <rdaneshvar95@gmail.com>
2021-07-18 02:10:01 +0200

    headerview aka promotion banner added

--- Brewmaster/Constants/AppColor.swift
+++ Brewmaster/Constants/AppColor.swift
@@ -9,4 +9,5 @@ import UIKit
 
 struct AppColor {
     static let background = UIColor(red: 11/255, green: 24/255, blue: 29/255, alpha: 1)
+    static let yellow = UIColor(red: 246/255, green: 175/255, blue: 66/255, alpha: 1)
 }
